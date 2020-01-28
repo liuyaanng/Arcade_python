28325326133a428315c0918dc4db5e4ebc7f91a7
liuyaanng <1757465118@qq.com>
2020-01-28 12:18:23 +0800

    basic_arcade

---
+++ basic_arcade.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Basic arcade program
+# Display a white window with a blue circle in the middle
+
+
+import arcade
+
+# Constants
+SCREEN_WIDTH = 600
+SCREEN_HEIGHT = 800
+SCREEN_TITLE = "Welcome to Arcade"
+RADIUS = 150
+
+# Open the window
+arcade.open_window(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+
+# Set the background color
+arcade.set_background_color(arcade.color.WHITE)
+
+# Clear the screen and start drawing
+arcade.start_render()
+
+# Draw a blue circle
+arcade.draw_circle_filled(
+    SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, RADIUS, arcade.color.BLUE
+)
+arcade.draw_rectangle_filled(SCREEN_WIDTH / 4, SCREEN_HEIGHT / 4, 50, 50,  arcade.color.YELLOW)
+
+# Finish drawing
+arcade.finish_render()
+
+# Display everything
+arcade.run()
+
